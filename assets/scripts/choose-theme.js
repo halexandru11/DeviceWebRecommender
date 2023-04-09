a4249483bd18be8b60537f7df4186db27644013f
halexandru11 <alxhri11@gmail.com>
2023-04-09 21:04:25 +0300

    Managed to let the user pick a colorscheme and apply it globally. Not done yet

--- assets/scripts/choose-theme.js
+++ assets/scripts/choose-theme.js
@@ -1,3 +1,29 @@
-const changeTheme = function() {
-    
+const colorThemes = document.querySelectorAll('[name="color"]');
+
+const storeTheme = (theme) => {
+    localStorage.setItem('theme', theme);
 }
+
+const getTheme = () => {
+    const theme = localStorage.getItem('theme');
+    return theme;
+}
+
+const retrieveTheme = function() {
+    const activeTheme = getTheme();
+    colorThemes.forEach((theme) => {
+        if (theme.id === activeTheme) {
+            theme.checked = true;
+        }
+    });
+    document.documentElement.setAttribute('data-theme', activeTheme);
+}
+
+colorThemes.forEach((theme) => {
+    theme.addEventListener('click', () => {
+        storeTheme(theme.id);
+        retrieveTheme();
+    });
+}); 
+
+document.onload = retrieveTheme();
