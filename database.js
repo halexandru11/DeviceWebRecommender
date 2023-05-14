f3803e0876c1e75148f4f4a2cbb4bcef0cee531d
Robert Rascanu <robert.rscn@gmail.com>
2023-05-14 23:24:43 +0300

    +Script for creating the database

--- database.js
+++ database.js
@@ -6,7 +6,8 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
+    connectionLimit: 10
 }).promise();
 
 // The code below should be removed
