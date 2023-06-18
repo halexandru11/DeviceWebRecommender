e9f9302949410dd1afea9a6c7eba8233cba1bcdd
Andreea Lupu <0607andreea.ro@gmail.com>
2023-06-18 15:01:35 +0300

    sign in almost done

--- endpoints/SignIn.js
+++ endpoints/SignIn.js
@@ -1,5 +1,7 @@
 import crypto from 'crypto';
 import querystring from 'querystring';
+import mysql from 'mysql2';
+
 const handleSignInPost = (req, res) => {
     try {
         let body = [];
@@ -17,13 +19,50 @@ const handleSignInPost = (req, res) => {
             const hashedPassword = crypto.createHash('sha256').update(password).digest('hex');
             console.log('Hashed password:', hashedPassword);
 
+            const connection = mysql.createConnection({
+                host: 'localhost',
+                user: 'root',
+                password: 'password',
+                database: 'gimme'
+            });
+
+            connection.connect();
+
+            const userSql = 'SELECT id FROM users WHERE username = ?';
+            connection.query(userSql, [username], (error, userResults) => {
+                if (error) {
+                    console.error('Error querying user:', error);
+                } else {
+                    if (userResults.length === 0) {
+                        console.log('User does not exist.');
+                        //Handle user does not exist case
+
+                    } else {
+                        const userId = userResults[0].id;
+                        const passwordSql = 'SELECT user_id FROM passwords WHERE user_id = ? AND password = ?';
+                        connection.query(passwordSql, [userId, hashedPassword], (error, passwordResults) => {
+                            if (error) {
+                                console.error('Error querying password:', error);
+                            } else {
+                                if (passwordResults.length === 0) {
+                                    console.log('Incorrect password.');
+                                    //Handle incorrect password case
+                                } else {
+                                    console.log('User authenticated successfully. User ID:', userId);
+                                    //Handle successful authentication
+
+                                }
+                            }
+                        });
+                    }
+                }
+            });
             res.writeHead(302, { 'Location': '../' });
             res.end();
         });
-    }
-    catch (err) {
+    } catch (err) {
         console.log(err);
     }
-
 };
-export { handleSignInPost }
+
+export { handleSignInPost };
