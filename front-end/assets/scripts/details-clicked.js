7fc0ccaf6fe9a8d72bcaaaf5590f442dad831dd9
Andreea Lupu <0607andreea.ro@gmail.com>
2023-06-21 19:39:55 +0300

    changes

---
+++ front-end/assets/scripts/details-clicked.js
@@ -0,0 +1,5 @@
+var detailsButton = document.getElementById("details-button");
+detailsButton.addEventListener("click", function () {
+    // Perform your tracking logic here
+    console.log("Details button clicked");
+});
